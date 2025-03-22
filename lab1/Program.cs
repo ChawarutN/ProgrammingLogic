538e864ae77880afec8db8feb5d85d7b786fa311
AI640 <dewza7789@gmail.com>
2025-03-22 06:06:43 +0000

    Created Lab 1

---
+++ lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("welcome to IT-1050!");
+    }
+}
