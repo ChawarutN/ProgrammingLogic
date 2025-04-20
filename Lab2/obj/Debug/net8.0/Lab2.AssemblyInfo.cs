afb79d2f01d50e6ae7006ca3c5a0f01ffcdde60c
AI640 <dewza7789@gmail.com>
2025-04-20 04:30:11 +0000

    Lab5

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d821dc99e88851248da56171a2c801af68bcf5c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713a604a1dae9718d3a73aa36276f6a1577c6479")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab5/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Lab5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Part 1: Seasons of the Year
+
+        string [] seasons = { "Spring", "Summer", "Fall", "Winter" };
+
+        foreach (string season in seasons)
+        {
+            Console.WriteLine($"Season: {season}");
+        }
+
+        //Part 2: Days of the Week
+
+        string [] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        Console.WriteLine("Enter a number (1-7): ");
+
+        if (int.TryParse(Console.ReadLine(), out int number) && number >= 1 && number <= 7)
+        {
+            Console.WriteLine($"That day is: {days[number - 1]}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 1 and 7");
+        }
+        
+        //Part 3: Favorite Books and Authors
+
+        string [] books = { "The Hobbit", "Charlotte's Web", "1984" };
+        string [] authors = { "J.R.R. Tolkien", "E.B White", "George Orwell" };
+
+        for (int i = 0; i < books.Length; i++)
+        {
+            Console.WriteLine($"Book {i + 1}: {books[i]} by {authors[i]}");
+        }
+
+        //Part 4: Temperature Tracker
+
+        int [] temperatures = { 65, 72, 78, 70, 68 };
+
+        Array.Sort(temperatures);
+
+        Console.Write("Sorted Temeratures: ");
+        foreach (int temp in temperatures)
+        {
+            Console.Write(temp + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Highest Temperature: {temperatures[temperatures.Length - 1]}");
+        Console.WriteLine($"Lowest Temperature: {temperatures[0]}");
+
+        //Part 5: Reverse Countdown
+
+        int [] countdown = { 5, 4, 3, 2, 1};
+
+        Array.Reverse(countdown);
+
+        Console.Write("Countdown: ");
+        for (int i = 0; i < countdown.Length; i++)
+        {
+            Console.Write(countdown[i] + " ");
+        }
+        Console.WriteLine();
+
+    }
+}
